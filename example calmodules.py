5f32f139a307cdf22176eb7dbd82bdcb316221f5
dikshakolhe2102 <dikshakolhe54@gmail.com>
2025-06-24 12:00:24 +0530

    cal modules import file

---
+++ example calmodules.py
@@ -0,0 +1,19 @@
+# import calmodules
+# print (calmodules.add(1,2))
+
+# import calmodules as a
+# print (a.add(2,2))
+
+# from calmodules import multiply
+# print(multiply(2,2))
+#print(add(1,2)) only import multiply function
+a=20
+b=30
+c=40
+from calmodules import *
+print(multiply(2,2))
+print(subtract(1,5))
+print(divide(10,2))
+print(add(1,2)) 
+print(a,b,c)
+
