563657582bcfb72178ae9c43d1440fa759d23db4
dikshakolhe2102 <dikshakolhe54@gmail.com>
2025-06-24 11:59:47 +0530

    modules

---
+++ calmodules.py
@@ -0,0 +1,15 @@
+a=10
+b=20
+c=50
+def add(a,b):
+    return a+b
+def subtract(a,b):
+
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+
+    if b==0:
+        return "error! Division by zero"
+    return a/b
